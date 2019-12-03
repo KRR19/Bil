900d05e8e9354e550a69a0730c8e9374f7ea94c1
Роман Казаров <kazarovroman@gmail.com>
2019-12-03 21:30:34 +0200

    Добавьте файлы проекта.

---
+++ DataLayer/ApplicationContext/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using DataLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer.ApplicationContext
+{
+    public class ApplicationContext: DbContext
+    {
+        public DbSet<Product> Products{ get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public ApplicationContext(DbContextOptions optionsBuilder) : base(optionsBuilder)
+        {
+
+        }
+    }
+}
---
+++ DataLayer/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer.Entities
+{
+    public class Category
+    {
+        public string Name { get; set; }
+    }
+}
